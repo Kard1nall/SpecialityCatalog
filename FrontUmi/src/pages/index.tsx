b8c2445a3a641f0ae84ad3da79ce1861169b6098
Kard1naL <prrsmm@yandex.ru>
2023-06-09 15:56:19 +0500

    Chart

--- FrontUmi/src/pages/index.tsx
+++ FrontUmi/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import ModalComponent from '@/components/ModalComponent';
 import LayoutAuth from '@/layouts/LayoutAuth';
-import { Line } from '@ant-design/charts';
+import { Line, Pie } from '@ant-design/charts';
 import { useModel } from '@umijs/max';
 import { Button, Modal } from 'antd';
 import React from 'react';
@@ -15,6 +15,63 @@ export default function HomePage() {
     console.log("Modal callback")
   };
 
+
+  const data = [
+    {
+      type: 'Бакалавриат',
+      value: 86,
+    },
+    {
+      type: 'Специалитет',
+      value: 2,
+    },
+    {
+      type: 'Магистратура',
+      value: 10,
+    },
+    {
+      type: 'Аспирантура',
+      value: 2,
+    },
+
+  ];
+  const config = {
+    appendPadding: 10,
+    data,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: 'inner',
+      offset: '-50%',
+      content: '{value}',
+      style: {
+        textAlign: 'center',
+        fontSize: 14,
+      },
+    },
+    interactions: [
+      {
+        type: 'element-selected',
+      },
+      {
+        type: 'element-active',
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: 'pre-wrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        },
+        content: 'Студенты',
+      },
+    },
+  };
+  
   
 
 
@@ -23,10 +80,10 @@ export default function HomePage() {
       <h2>Welcome, {initialState?.name}! </h2>
       <p>
 
-       
+      
       </p>
       <div>
-
+      <Pie {...config} />
       </div>
     </LayoutAuth>
   );
